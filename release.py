8b4e377fac4668550d42baacf4bf24418eb7cccd
athrvvv <codingbruh.ac@gmail.com>
2022-08-25 12:14:24 +0530

    Added good things :)

--- release.py
+++ release.py
@@ -3,7 +3,7 @@ from __future__ import print_function, unicode_literals, absolute_import
 name = 'AppOpener'
 
 branch = ''
-version = '1.1'
+version = '1.0'
 description = "Open any application by it's name."
 a = open("AppOpener/README.md","r")
 read = a.read()
@@ -37,7 +37,5 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                ]
 
 install_requires=[
-                "setuptools>=61.0.0",
-                "wheel",
-                "json"
+                "wheel"
     ]
