d2684800ba069f421b60972551b5299c83a4ea8d
Athrv Chaulkar <codingbruh.ac@gmail.com>
2023-04-30 03:44:40 -0700

    Added Example for using throwing Error

---
+++ Examples/Throw_Error.py
@@ -0,0 +1,5 @@
+from AppOpener import open, close
+
+open("APPNAME", throw_error=True) # Throws error and raises Exception when AppName is not found
+
+close("APPNAME", throw_error=True) # Throws error and raises Exception when AppName is not found running
